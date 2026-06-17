c0cfd88f67793a2b403c56b76b4f6b21646c770e
TLHolm144 <tlholm2006@gmail.com>
2026-06-17 11:27:23 -0500

    Fixed the photon trapping in radi_cal by introducing more physically accurate tyvek surface logic.

--- radi_cal/sim_run.sh
+++ radi_cal/sim_run.sh
@@ -6,7 +6,7 @@
 
 WORLD=radi_cal    # world module name
 PARTICLE=e-            # primary particle type (e.g., proton, electron, gamma)
-ENERGY_KEV=50000000         # energy in keV (200000 = 200 MeV)
+ENERGY_KEV=20000000         # energy in keV (200000 = 200 MeV)
 N_PARTICLES=100        # particles per run
 THREADS=8               # CPU threads per run
 N_RUNS=1                # number of runs
