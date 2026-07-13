a9d61e81e568afb9b078e51496454cb20e36080e
TLHolm144 <tlholm2006@gmail.com>
2026-07-13 11:19:40 -0500

    Debug

--- 12_sweep_analysis.py
+++ 12_sweep_analysis.py
@@ -45,7 +45,6 @@ REFRACTIVE_INDEX = {
     "luagce_rc_hex_triple":   1.84,
 }
 
-V_LIGHT_MM_NS = C_LIGHT_MM_NS / REFRACTIVE_INDEX
 
 BOUNCE_FACTOR = {
     "radi_cal_energy":        0.92,
@@ -77,8 +76,7 @@ T_OFFSET_NS = {
     "luagce_rc_hex_triple":   0.0,
 }
 
-for mod in BOUNCE_FACTOR.keys():
-    V_EFF_MM_NS = V_LIGHT_MM_NS * BOUNCE_FACTOR.get(mod, 0.92)
+
 
 _GT_LO_NS = 0.0
 _GT_HI_NS = 50.0
@@ -249,7 +247,7 @@ def analyze_energy_batch(batch_dir: Path, is_hex: bool, module_name: str, verbos
         return None
 
     lyso_thick = _KNOWN_MODULE_LYSO_THICK[module_name]
-    v_light = v_eff_for_module(mod)
+    
     v_eff = v_eff_for_module(module_name)
     t_offset_ns = T_OFFSET_NS.get(module_name, 0.0)
    
