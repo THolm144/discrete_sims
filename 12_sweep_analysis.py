19c554c91dc5e200cf637f5cfaae69c64ce145ad
TLHolm144 <tlholm2006@gmail.com>
2026-07-13 15:44:37 -0500

    debug

--- 12_sweep_analysis.py
+++ 12_sweep_analysis.py
@@ -45,7 +45,7 @@ REFRACTIVE_INDEX = {
     "luagce_rc_hex_triple":   1.84,
 }
 
-V_LIGHT_MM_NS = C_LIGHT_MM_NS / REFRACTIVE_INDEX
+
 
 
 BOUNCE_FACTOR = {
@@ -78,8 +78,8 @@ T_OFFSET_NS = {
     "luagce_rc_hex_triple":   0.0,
 }
 
-for mod in BOUNCE_FACTOR.keys():
-    V_EFF_MM_NS = V_LIGHT_MM_NS * BOUNCE_FACTOR.get(mod, 0.92)
+
+
 
 
 
