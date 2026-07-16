a53843e4260ffdd9952b2e2c7bdad6cd9e9a10d3
TLHolm144 <tlholm2006@gmail.com>
2026-07-16 17:08:34 -0500

    I underestimated my own cluster

--- luagce_radi_cal_triple/run_energy_sweep.sh
+++ luagce_radi_cal_triple/run_energy_sweep.sh
@@ -9,7 +9,7 @@ PHYSICS_LIST="QGSP_BERT_EMV"
 # Dynamically balanced totals calculated for a 16-hour execution safety window
 # Spawns 1 process per energy x 10 internal C++ threads = 40 cores maxed out
 ENERGIES=(25000000 50000000 70000000 90000000)
-COUNTS=(1850 935 660 510)
+COUNTS=(18500 9350 6600 5100)
 THREADS=10
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
