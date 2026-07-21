4f72ec712a0c6594dfe45622004b2d0006205b2a
TLHolm144 <tlholm2006@gmail.com>
2026-07-21 10:45:40 -0500

    added 4 more energy points to the dsb1 radical energy sweep. i will run that and then combine the data weith the previous sweep.

--- dsb1_radi_cal_energy/run_energy_sweep.sh
+++ dsb1_radi_cal_energy/run_energy_sweep.sh
@@ -8,7 +8,7 @@ PHYSICS_LIST="QGSP_BERT_EMV"
 
 # Dynamically balanced totals calculated for a 16-hour execution safety window
 # Spawns 1 process per energy x 10 internal C++ threads = 40 cores maxed out
-ENERGIES=(25000000 50000000 70000000 90000000)
+ENERGIES=(10000000 30000000 40000000 80000000 )
 COUNTS=(400 280 200 80)
 THREADS=100  # threads per process, not total threads. Total threads = THREADS * number of processes
 
