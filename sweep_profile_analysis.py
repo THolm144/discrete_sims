33450652024790039a2658d4639e876a030d66d3
TLHolm144 <tlholm2006@gmail.com>
2026-07-24 14:35:04 -0500

    set the profile analysis to focus on just dsb1_radi_cal_energy

--- sweep_profile_analysis.py
+++ sweep_profile_analysis.py
@@ -635,8 +635,8 @@ def main():
     base_dir = Path(__file__).resolve().parent
     modules = [
         #"radi_cal_energy", "radi_cal_triple", "rc_hex", "rc_hex_triple",
-        "dsb1_radi_cal_energy", "dsb1_radi_cal_triple", "dsb1_rc_hex", "dsb1_rc_hex_triple",
-        "luagce_radi_cal_energy", "luagce_radi_cal_triple", "luagce_rc_hex", "luagce_rc_hex_triple"
+        "dsb1_radi_cal_energy", #"dsb1_radi_cal_triple", "dsb1_rc_hex", "dsb1_rc_hex_triple",
+        #"luagce_radi_cal_energy", "luagce_radi_cal_triple", "luagce_rc_hex", "luagce_rc_hex_triple"
     ]
 
     out_dir = base_dir / "profile_analysis"
